c2099f350069347383d04babc73be74189043e47
Leslie Reyes <lesliereyesv@gmail.com>
2023-07-04 19:46:59 -0400

    add js bootstrap, sass folder, cardbook, page books

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Books from "./pages/Books";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <>
       <Navbar></Navbar>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+        </Routes>
+      </main>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,6 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import "./assets/css/style.css"
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
