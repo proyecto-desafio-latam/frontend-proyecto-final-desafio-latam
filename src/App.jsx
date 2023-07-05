e7e655e150673ff69ac84d8152ec394d03b5b9e6
Rodrigo Gonzalez <rodrigo.gonzaleztr@outlook.com>
2023-07-05 17:09:28 -0400

    merge de main sin formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Books from "./pages/Books";
+import Home from "./pages/Home";
+import BookDetail from "./pages/BookDetail";
+
 export default function App() {
   return (
     <>
+      <Navbar></Navbar>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/books/:id" element={<BookDetail />} />
+        </Routes>
+      </main>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-// import './index.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import "./assets/css/style.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import UserContextProvider from "./context/UserContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
