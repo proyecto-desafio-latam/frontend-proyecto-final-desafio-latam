c3ec9109ff44b773ecca672b665ffb9a8b11b076
Rodrigo Gonzalez <rodrigo.gonzaleztr@outlook.com>
2023-07-05 15:48:01 -0400

    Formulario de registro, validaciones, downgrade de bootstrap, install formik package

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,105 +1,228 @@
 import React, { useState } from 'react';
+import { Field, Formik, Form, ErrorMessage } from 'formik';
+import '../Register.css';
 
 export default function Register() {
-    const [nombre, setNombre] = useState('');
-    const [apellido, setApellido] = useState('');
-    const [username, setUsername] = useState('');
-    const [correo, setCorreo] = useState('');
-    const [contrasena, setContrasena] = useState('');
-    const [repetirContrasena, setRepetirContrasena] = useState('');
-    const [fechaNacimiento, setFechaNacimiento] = useState('');
 
-    const handleSubmit = async (event) => {
-
-        // const fechaFormateada = new Date(fechaNacimiento).toLocaleDateString('es-ES', {
-        //     day: '2-digit',
-        //     month: 'long',
-        //     year: 'numeric',
-        // });
-
-        event.preventDefault();
-        // Mostrar los valores en la consola
-
-
-        console.log('Valores del formulario:');
-        console.log('Nombre:', nombre);
-        console.log('Apellido:', apellido);
-        console.log('Nombre de usuario:', username);
-        console.log('Correo:', correo);
-        console.log('Contraseña:', contrasena);
-        console.log('Repita contraseña:', repetirContrasena);
-        console.log('Fecha de nacimiento:', fechaNacimiento);
-
-        // Aquí puedes realizar acciones adicionales, como enviar los datos al servidor
-        console.log('Formulario enviado');
-
-
-        // Construir el objeto de datos a enviar
-        const data = {
-            nombre,
-            apellido,
-            username,
-            correo,
-            contrasena,
-            repetirContrasena,
-            fechaNacimiento,
-        };
-
-        try {
-            // Enviar la solicitud POST al backend
-            const response = await fetch('/ruta-del-backend', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-            });
-
-            const responseData = await response.json();
-            console.log('Respuesta del servidor:', responseData);
-
-            // Aquí puedes realizar acciones adicionales después de enviar los datos
-
-        } catch (error) {
-            console.error('Error al enviar los datos:', error);
-            // Aquí puedes manejar el error de acuerdo a tus necesidades
-        }
-    };
-
-    const handleLimpiar = () => {
-        setNombre('');
-        setApellido('');
-        setUsername('');
-        setCorreo('');
-        setContrasena('');
-        setRepetirContrasena('');
-        setFechaNacimiento('');
-    };
+    const [submittedForm, setSubmittedForm] = useState(false);
 
     return (
-        <div id="container-login">
-            <form onSubmit={handleSubmit}>
-                <p>Nombre:</p>
-                <input type="text" placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
-                <p>Apellido:</p>
-                <input type="text" placeholder="Apellido" value={apellido} onChange={(e) => setApellido(e.target.value)} />
-                <p>Nombre de usuario:</p>
-                <input type="text" placeholder="Nombre de usuario" value={username} onChange={(e) => setUsername(e.target.value)} />
-                <p>Correo:</p>
-                <input type="email" placeholder="correo@correito.com" value={correo} onChange={(e) => setCorreo(e.target.value)} />
-                <p>Contraseña:</p>
-                <input type="password" placeholder="******" value={contrasena} onChange={(e) => setContrasena(e.target.value)} />
-                <br />
-                <p>Repita contraseña:</p>
-                <input type="password" placeholder="******" value={repetirContrasena} onChange={(e) => setRepetirContrasena(e.target.value)} />
-                <p>Fecha de nacimiento:</p>
-                <input type="date" placeholder="" value={fechaNacimiento} onChange={(e) => setFechaNacimiento(e.target.value)} />
-                <br />
-                <div>
-                    <button type="button" onClick={handleLimpiar}>Limpiar</button>
-                    <button type="submit">Registrarse</button>
-                </div>
-            </form>
-        </div>
+        <>
+            <Formik
+                initialValues={{
+                    firstName: '',
+                    lastName: '',
+                    userName: '',
+                    email: '',
+                    password: '',
+                    repeatPassword: '',
+                    birthday: ''
+                }}
+
+                validate={(values) => {
+
+                    let validations = {};
+
+                    // Validación firstName
+                    if (!values.firstName) {
+                        validations.firstName = 'Por favor ingresa un nombre';
+                    } else if (!/^[a-zA-ZáéíóúñÁÉÍÓÚÑ\s]+$/.test(values.firstName)) {
+                        validations.firstName = 'El nombre solo puede contener letras';
+                    } else if (values.firstName.length < 2) {
+                        validations.firstName = 'Ingrese un nombre válido';
+                    }
+
+
+                    // Validación lastName
+                    if (!values.lastName) {
+                        validations.lastName = 'Por favor ingresa un apellido';
+                    } else if (!/^[a-zA-ZáéíóúñÁÉÍÓÚÑ\s]+$/.test(values.lastName)) {
+                        validations.lastName = 'El apellido solo puede contener letras';
+                    } else if (values.lastName.length < 2) {
+                        validations.lastName = 'Ingrese un apellido válido';
+                    }
+
+
+                    // Validación userName
+                    if (!values.userName) {
+                        validations.userName = 'Por favor ingresa un nombre de usuario válido';
+                    } else if (!/^[a-zA-Z0-9.\-_]+$/.test(values.userName)) {
+                        validations.userName = 'El nombre de usuario solo puede contener letras';
+                    } else if (values.userName.length < 2) {
+                        validations.userName = 'Ingrese un nombre de usuario válido';
+                    }
+
+                    // Validación email
+                    if (!values.email) {
+                        validations.email = 'Por favor ingresa un correo válido';
+                    } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.email)) {
+                        validations.email = 'Por favor ingresa un correo válido';
+                    }
+
+                    // Password Validation
+                    if (!values.password) {
+                        validations.password = 'Por favor ingrese una contraseña';
+                    } else if (values.password.length < 6) {
+                        validations.password = 'La contraseña es demasiado corta';
+                    }
+
+                    // RepeatPassword Validation
+                    if (!values.repeatPassword) {
+                        validations.repeatPassword = 'Por favor repita la contraseña correctamente';
+                    } else if (values.password !== values.repeatPassword) {
+                        validations.repeatPassword = 'Las contraseñas no coinciden';
+                    }
+
+                    //Birthday Validation
+                    const minAgeDate = new Date();
+                    minAgeDate.setFullYear(minAgeDate.getFullYear() - 18);
+                    const selectedDate = new Date(values.birthday);
+
+                    if (selectedDate > minAgeDate) {
+                        validations.birthday = 'Debes tener al menos 18 años';
+                    }
+                    return validations;
+                }}
+
+                onSubmit={async (values, { resetForm }) => {
+
+                    try {
+                        const response = await fetch('/api/register', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(values)
+
+                        });
+
+                        console.log(values)
+
+
+                        if (response.ok) {
+                            // If register is success
+
+                            resetForm();             // clean inputs
+                            setSubmittedForm(true);  // change state for "Registro exitoso"
+                            setTimeout(() => setSubmittedForm(false), 4000); //hide "Registro exitoso after 4 sec"
+                        } else {
+                            // If is an error server
+                            console.error('Error en el servidor:', response.status);
+                        }
+                    } catch (error) {
+                        // Error in request
+                        console.error('Error al enviar la solicitud:', error);
+                    }
+                }}
+            >
+
+                {({ errors }) => (
+                    <Form className='register-form'>
+                        <div className='form-field'>
+                            <label className='form-label' htmlFor="firstName">Nombre</label>
+                            <Field
+                                className="form-input"
+                                type="text"
+                                id='firstName'
+                                name='firstName'
+                                placeholder='Susan'
+                            />
+                            <ErrorMessage name='firstName' component={() => (
+                                <div className='form-error'>{errors.firstName}</div>
+                            )} />
+
+                        </div>
+                        <div className='form-field'>
+                            <label className='form-label' htmlFor="lastName">Apellido</label>
+                            <Field
+                                className="form-input"
+                                type="text"
+                                id='lastName'
+                                name='lastName'
+                                placeholder='Navarro'
+                            />
+                            <ErrorMessage className='form-error' name='lastName'
+                                component={() => (
+                                    <div className='form-error'>{errors.lastName}</div>
+                                )} />
+
+                        </div>
+                        <div className='form-field'>
+                            <label className='form-label' htmlFor="name">Nombre de usuario</label>
+                            <Field
+                                className="form-input"
+                                type="text"
+                                id='userName'
+                                name='userName'
+                                placeholder='suuuuu'
+                            />
+                            <ErrorMessage
+                                className='form-error'
+                                name='userName'
+                                component={() => (
+                                    <div className='form-error'>{errors.userName}</div>
+                                )} />
+                        </div>
+                        <div className='form-field'>
+                            <label className='form-label' htmlFor="name">Correo</label>
+                            <Field
+                                className="form-input"
+                                type="email"
+                                id='email'
+                                name='email'
+                                placeholder='correo@correo.com'
+                            />
+                            <ErrorMessage name='email'
+                                component={() => (
+                                    <div className='form-error'>{errors.email}</div>
+                                )} />
+                        </div>
+                        <div className='form-field'>
+                            <label className='form-label' htmlFor="password">Contraseña</label>
+                            <Field
+                                className="form-input"
+                                type="password"
+                                id='password'
+                                name='password'
+                                placeholder='********'
+                            />
+                            <ErrorMessage name='password'
+                                component={() => (
+                                    <div className='form-error'>{errors.password}</div>
+                                )} />
+                        </div>
+                        <div className='form-field'>
+                            <label className='form-label' htmlFor="repeatPassword">Repetir contraseña</label>
+                            <Field
+                                className="form-input"
+                                type="password"
+                                id='repeatPassword'
+                                name='repeatPassword'
+                                placeholder='********'
+                            />
+                            <ErrorMessage name='repeatPassword'
+                                component={() => (
+                                    <div className='form-error'>{errors.repeatPassword}</div>
+                                )} />
+                        </div>
+                        <div className='form-field'>
+                            <label className='form-label' htmlFor="birthday">Fecha de nacimiento</label>
+                            <Field
+                                className="form-input form-input--date"
+                                type="date"
+                                id='birthday'
+                                name='birthday'
+                            />
+                            <ErrorMessage name='birthday'
+                                component={() => (
+                                    <div className='form-error'>{errors.birthday}</div>
+                                )} />
+                        </div>
+                        <button className='form-button' type="submit">Registrar</button>
+                        {submittedForm && <p className="success-message">Registro exitoso</p>}
+                    </Form>
+                )}
+            </Formik >
+        </>
     );
 }
