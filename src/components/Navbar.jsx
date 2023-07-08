b2da0851f90596602e1f1cd8fa34a2fee922dcd4
Matt Cuadros <mcuadrose@gmail.com>
2023-07-07 21:55:06 -0400

    fix: Navbar modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
       data-scroll-header
     >
       <div className="container-fluid pe-lg-4">
-        <div className="d-flex align-items-center">
+        <div className="d-flex align-items-around w-100">
           <a href="" className="navbar-brand flex-shrink-0 py-1 py-lg-2">
             <img src="../ejemplo-logo.jpg" width="47" alt="Logo" />
             Libros
