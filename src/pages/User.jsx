02c87c4465d273529e46688fbd4055a672b9f64f
Matt Cuadros <mcuadrose@gmail.com>
2023-07-12 01:42:32 -0400

    fix: User.jsx modified

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,34 +1,70 @@
+import { Avatar } from "@mui/material";
 import "../assets/css/User.css";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 const User = () => {
   return (
     <>
-      <div className="container-fluid mt-5 pt-5">
-        <span>Mi Perfil</span>
-        <div className="rounded">Imagen</div>
-        <div className="user-table d-flex align-items-baseline">
-          <i className="ri-user-line"></i>
-          <span>Nombre:</span>
-          <h3>Matias</h3>
-          <span className="bar"></span>
-        </div>
-        <div className="user-table d-flex align-items-baseline">
-          <i className="ri-user-line"></i>
-          <span>Apellido:</span>
-          <h3>Cuadros</h3>
-          <span className="bar"></span>
-        </div>
-        <div className="user-table d-flex align-items-baseline">
-          <i className="ri-mail-line"></i>
-          <span>Email:</span>
-          <h3>mcuadrose@gmail.com</h3>
-          <span className="bar"></span>
-        </div>
-        <div className="user-table d-flex align-items-baseline">
-          <i className="ri-mail-line"></i>
-          <span>Usuario desde:</span>
-          <h3>10-04-2022</h3>
-          <span className="bar"></span>
+      <div className="container-fluid user-table mt-5 pt-5">
+        <div className=" d-flex flex-column">
+          <div className="d-flex align-items-center justify-content-center me-5">
+            <Avatar
+              alt="Avatar"
+              src="/avatar.png"
+              sx={{ width: 56, height: 56 }}
+            />
+            <span className="mt-3 ps-3 display-6 text-center">Mi Perfil</span>
+          </div>
+
+          <div className="d-flex mt-5 pt-2">
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { m: 1, width: "30vw" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <div className="d-flex flex-column w-50">
+                <TextField
+                  disabled
+                  fullWidth
+                  id="outlined-disabled"
+                  label="Nombre"
+                  defaultValue="Matias"
+                />
+                <TextField
+                  disabled
+                  id="outlined-disabled"
+                  label="Apellido"
+                  defaultValue="Cuadros"
+                />
+                <TextField
+                  id="outlined-read-only-input"
+                  label="Usuario"
+                  defaultValue="mcuadrose"
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                />
+                <TextField
+                  disabled
+                  id="outlined-disabled"
+                  label="Correo"
+                  defaultValue="mcuadrose@gmail.com"
+                />
+                <TextField
+                  id="outlined-read-only-input"
+                  label="Fecha Nacimiento"
+                  defaultValue="04-10-1988"
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                />
+              </div>
+            </Box>
+          </div>
         </div>
       </div>
     </>
