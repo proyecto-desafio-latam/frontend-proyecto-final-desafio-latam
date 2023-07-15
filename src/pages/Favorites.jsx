8d3d0956973ddb6dbbe2091945051d3c17c0feb0
Leslie Reyes <lesliereyesv@gmail.com>
2023-07-15 11:16:38 -0400

    changes at Favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import { BiSolidHeart } from "react-icons/bi";
+import { useAuthContext } from "../context/AuthContext";
 import ListFav from "../components/ListFav";
 
 
 const Favorites = () => {
+  const { favorites } = useAuthContext()
+
+  if (favorites.length === 0) return (
+    <div className="container  m-5 pt-5 vh-50">
+      <h2 className="text-center pt-3"> <BiSolidHeart style={{ color: 'red' }} /> Mis Libros Favoritos <BiSolidHeart style={{ color: 'red' }} /> </h2>
+      <div  >
+        <p className="text-center fs-md mt-5">Aún no tienes favoritos, para poder agregarlos, accede al catálogo de libros <Link to="/books">aquí</Link></p>
+      </div>
+    </div>
+  )
+
+
   return (
     <>
       <ListFav></ListFav>
