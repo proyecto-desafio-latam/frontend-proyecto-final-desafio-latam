6682fa27f0d472e059834312c399aad26cfe38d3
Matt Cuadros <mcuadrose@gmail.com>
2023-07-23 17:10:51 -0400

    feat: conection client + backend successfull in BookContext

--- src/context/BookContext.jsx
+++ src/context/BookContext.jsx
@@ -1,33 +1,41 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-export const BookContext = createContext()
+export const BookContext = createContext();
 
 export default function BookContextProvider({ children }) {
-    const [books, setBooks] = useState([])
-    const [error, setError] = useState()
+  const [books, setBooks] = useState([]);
+  const [error, setError] = useState();
 
-    const FormatCoin = (number) => 
-    ( new Intl.NumberFormat('es-CL', { style: 'currency', currency: 'CLP' }).format(number))
+  const FormatCoin = (number) =>
+    new Intl.NumberFormat("es-CL", {
+      style: "currency",
+      currency: "CLP",
+    }).format(number);
 
-    const getData = async () => {
-        try {
-            const response = await fetch("/inventory.json")
-            if (!response.ok) throw "No se puede desplegar la información"
-            const data = await response.json()
-            setBooks(data)
-        } catch (error) {
-            setError(error)
-        }
+  const getData = async () => {
+    try {
+      const connectionString = "https://node-bookstore-ww7n.onrender.com/";
+      const response = await fetch(connectionString + "api/v1/books");
+      if (!response.ok) throw "No se puede desplegar la información";
+      console.log(response);
+      const { result } = await response.json();
+      console.log(result);
+      setBooks(result);
+    } catch (error) {
+      setError(error);
     }
-    useEffect(() => {
-        getData();
-    }, [])
+  };
+  useEffect(() => {
+    getData();
+  }, []);
 
-    return(
-        <BookContext.Provider value={{books, setBooks, error, setError, FormatCoin}}>
-            {children}
-        </BookContext.Provider>
-    )
+  return (
+    <BookContext.Provider
+      value={{ books, setBooks, error, setError, FormatCoin }}
+    >
+      {children}
+    </BookContext.Provider>
+  );
 }
 
 export const useBookContext = () => useContext(BookContext);
