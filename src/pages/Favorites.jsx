28fc7fd66987e192c7f3d25d220ea7f46e29e40a
Leslie Reyes <lesliereyesv@gmail.com>
2023-07-24 23:16:07 -0400

    center div at favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -8,7 +8,7 @@ const Favorites = () => {
   const { favorites } = useAuthContext()
 
   if (favorites.length === 0) return (
-    <div className="container  m-5  vh-50">
+    <div className="container pb-5 pt-2 vh-50">
       <h2 className="text-center pt-3"> <BiSolidHeart style={{ color: 'red' }} /> Mis Libros Favoritos <BiSolidHeart style={{ color: 'red' }} /> </h2>
       <div  >
         <p className="text-center fs-md mt-5">Aún no tienes favoritos, para poder agregarlos, accede al catálogo de libros <Link to="/books">aquí</Link></p>
